95012724a1db00a777a4b3b966b63b063a5a9472
Sristi Sharma <sristis281@gmail.com>
2026-05-28 13:14:37 +0530

    Enhanced todo app with delete functionality

--- to_do/script.js
+++ to_do/script.js
@@ -2,11 +2,34 @@ function addTask(){
 
     let input = document.getElementById("taskInput");
 
+    if(input.value === ""){
+        alert("Please enter a task");
+        return;
+    }
+
     let li = document.createElement("li");
 
     li.innerText = input.value;
+
+    let deleteBtn = document.createElement("button");
+
+    deleteBtn.innerText = "Delete";
+
+    deleteBtn.onclick = function(){
+        li.remove();
+    }
+
+    li.appendChild(deleteBtn);
 
     document.getElementById("taskList").appendChild(li);
 
     input.value = "";
 }
+
+document.getElementById("taskInput")
+.addEventListener("keypress", function(event){
+
+    if(event.key === "Enter"){
+        addTask();
+    }
+});
