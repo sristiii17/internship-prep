0ab281e812cd8fe6aaee98f73694d0e79471905d
Sristi Sharma <sristis281@gmail.com>
2026-05-29 17:43:06 +0530

    build a basic calculator

---
+++ calculator/script.js
@@ -0,0 +1,16 @@
+function appendValue(value){
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay(){
+    document.getElementById("display").value = "";
+}
+
+function calculate(){
+
+    let expression = document.getElementById("display").value;
+
+    let result = eval(expression);
+
+    document.getElementById("display").value = result;
+}
